d8f0c0752b4d5b7637f82ea47d1e37a889f5dd05
Dinner <92211840+dinnermc@users.noreply.github.com>
2026-07-20 17:55:06 +0200

    Fix dedicated server crash: isolate client classes to src/client/java

--- src/main/java/dinner/dev/endless_gravity/EndlessGravity.java
+++ src/main/java/dinner/dev/endless_gravity/EndlessGravity.java
@@ -4,7 +4,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
@@ -15,11 +14,6 @@ public class EndlessGravity {
 
     public EndlessGravity(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.COMMON_SPEC);
-
-        modContainer.registerExtensionPoint(
-                IConfigScreenFactory.class,
-                (mc, parent) -> new EndlessGravityConfigScreen(parent)
-        );
 
         LOGGER.info("Endless Gravity loaded");
     }
